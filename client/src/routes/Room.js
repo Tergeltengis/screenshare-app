248b0187b928c66c573f4d00c6b1d82a867e56b9
tergeltengis <tergeltengis77@gmail.com>
2023-01-04 10:26:12 +0800

    feature screen share

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -29,10 +29,10 @@ const Video = (props) => {
   return <StyledVideo playsInline autoPlay ref={ref} />;
 };
 
-const videoConstraints = {
-  height: window.innerHeight / 2,
-  width: window.innerWidth / 2,
-};
+// const videoConstraints = {
+//   height: window.innerHeight / 2,
+//   width: window.innerWidth / 2,
+// };
 
 const Room = (props) => {
   const [peers, setPeers] = useState([]);
@@ -42,16 +42,18 @@ const Room = (props) => {
   const roomID = props.match.params.roomID;
 
   useEffect(() => {
-    socketRef.current = io?.connect("https://172.19.100.31:8000/");
+    socketRef.current = io?.connect(
+      "https://screenshare-app-production.up.railway.app/"
+    );
     console.log(navigator);
     navigator.mediaDevices
-      .getUserMedia({ video: videoConstraints, audio: true })
+      .getDisplayMedia({ video: true })
       .then((stream) => {
         console.log("media");
         userVideo.current.srcObject = stream;
         socketRef.current.emit("join room", roomID);
         socketRef.current.on("all users", (users) => {
-          console.log(users);
+          console.log("users", users);
           const peers = [];
           users.forEach((userID) => {
             const peer = createPeer(userID, socketRef.current.id, stream);
